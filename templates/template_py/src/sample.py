d3fa87cd13414133483030fccf4a1e7eae75aa94
Koreggo <101518891+Koreggo@users.noreply.github.com>
2022-05-06 00:06:24 +0900

    Update sample.py

--- templates/template_py/src/sample.py
+++ templates/template_py/src/sample.py
@@ -1,7 +1,24 @@
-import sys
-import math
-
-N = int(input())
-a, b = map(int, input().split())
-A = list(map(int, input().split()))
-AB = [list(map(int, input().split())) for _ in range(N)]
+import sys, re
+from collections import deque, defaultdict, Counter
+from math import ceil, floor, sqrt, hypot, factorial, pi, sin, cos, tan, asin, acos, atan, atan2, radians, degrees, log2, gcd
+from itertools import accumulate, permutations, combinations, combinations_with_replacement, product, groupby
+from functools import cmp_to_key
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from bisect import bisect, bisect_left, insort, insort_left
+from heapq import heappush , heappop
+from functools import reduce, lru_cache
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int, input().split()))
+def TUPLE(): return tuple(map(int, input().split()))
+def ZIP(n): return zip(*(MAP() for _ in range(n)))
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+#mod = 10 ** 9 + 7
+mod = 998244353
+from decimal import *
+#import numpy as np
+#decimal.getcontext().prec = 101
